735b4f3f93976d4b33698bf8159223fa3c115882
lvpengwei <lvpengwei@yuewen.com>
2018-12-15 16:54:11 +0800

    Initial Commit

---
+++ LottieInVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieInVideo
+//
+//  Created by lvpengwei on 2018/12/15.
+//  Copyright © 2018 lvpengwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
